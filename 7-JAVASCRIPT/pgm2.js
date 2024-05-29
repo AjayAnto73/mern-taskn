15828759d1d8a207a90ed06b5c6a42da9c7e2b21
unknown <AjayAnto73>
2024-05-29 14:49:46 +0530

    first commit

---
+++ 7-JAVASCRIPT/pgm2.js
@@ -0,0 +1,12 @@
+var name="ajay";
+console.log(name);
+console.log(typeof name)
+var age= 28
+console.log(typeof age);
+
+var isminor = true
+console.log(typeof isminor);
+var profession;
+console.log(typeof profession);
+var carname = null;
+console.log();
